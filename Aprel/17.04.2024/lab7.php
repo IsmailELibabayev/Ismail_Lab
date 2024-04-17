d88887b2d28f73a759e01d614be29f8720f49dd1
Ismail <isielibabayev@gmail.com>
2024-04-17 23:41:21 +0400

    Lab 7-8 elave edildi

---
+++ Aprel/17.04.2024/lab7.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+    <form action="C:\Users\Lunatic\Desktop\Ismail_Lab\Aprel\test.html">
+        <fieldset>
+            <legend></legend>
+           <h2> <label for="enter">Məlumatlarınızı daxil edin:</label><br><br></h2>
+            <label for="FN">Ad:</label>
+            <span>*</span><input type="text" id="FN" name="FN"><br><br>
+            <label for="LN">Soyad:</label>
+            <span>*</span><input type="text" id="LN" name="LN"><br><br>
+            <label for="SA">Küçə:</label>
+            <input type="text" id="SA" name="SA"><br><br>
+            <label for="T/C">Şəhər:</label>
+            <input type="text" id="T/C" name="T/C"><br><br>
+            <label for="P/Z">Postkod:</label>
+            <input type="text" id="P/Z" name="P/Z"><br><br>
+            <label for="T">Telefon:</label>
+            <input type="text" id="T" name="T"><br><br>
+            <label for="M">Mobil Telefon:</label>
+            <input type="text" id="M" name="M"><br><br>
+            <label for="E">Email:</label>
+            <input type="email" id="E" name="E"><br><br>
+            <p>* ilə işarələnmiş sahələr tələb olunur</p>
+            <input class="submit" type="submit" value="Qeydiyyat">
+        </fieldset>
+    </form>
+</body>
+</html>
